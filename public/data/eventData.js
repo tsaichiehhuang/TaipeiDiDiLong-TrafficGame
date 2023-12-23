974c9399fd90cddef972bb91ff1b203bd90a89e3
pj <Pinjiang1220@gmail.com>
2023-12-23 15:15:45 +0800

    Add managers
    - EventManager with eventData.js manages events
    - GameManger manages main things

---
+++ public/data/eventData.js
@@ -0,0 +1,37 @@
+const EventType = {
+  LEVEL: 'level', // 關卡
+  REPORT: 'report', // 檢舉
+  QA: 'qa', // 情境題
+  OTHER: 'other' // 其他
+};
+
+// [EventIDs]
+
+// Section 1
+const EVENT_REPORT_RED_LINE_PARKING = 'event_id_report_red_line_parking';
+const EVENT_LEVEL_TRAFFIC_LIGHT = 'event_id_level_traffic_light';
+const EVENT_QA_FLOWER_SELLER = 'event_id_flower_seller';
+
+// Section2
+// etc...
+
+const _eventsMap = {
+  [EVENT_LEVEL_TRAFFIC_LIGHT]: {
+    "EventName": "關卡_等紅燈", // Human readable name
+    "EventType": EventType.LEVEL,
+    "SuccessScore": 1,
+    "FailScore": -1,
+  },
+  [EVENT_REPORT_RED_LINE_PARKING]: {
+    "EventName": "檢舉_紅線停車",
+    "EventType": EventType.REPORT,
+    "SuccessScore": 1,
+    "FailScore": -0.5
+  },
+  [EVENT_QA_FLOWER_SELLER]: {
+    "EventName": "情境_玉蘭花",
+    "EventType": EventType.QA,
+    "SuccessScore": 1,
+    "FailScore": -1
+  }
+};
--- public/manager/gameManager.js
+++ public/manager/gameManager.js
@@ -1,65 +1,114 @@
+const GAME_WIDTH = 1280;
+const GAME_HEIGHT = 720;
+
+// Control the background and section
 class GameManager {
-  constructor() {
-    this._roadSprites = [];
-    this._roadImage = null;
-    this._roadHeight = 600; // road image height
-    this._backgroundSpeed = 0;
-  }
+	constructor() {
+		this._roadSprites = [];
+		this._roadImage = null;
+		this._roadHeight = 600; // road image height
+		this._section = 1; // 1-based, from 1 ~ 5
+		this._sectionChangedCallbacks = [];
+	}
 
-  preload = () => {
-    this._roadImage = loadImage("images/road/road_bg.png");
-  };
+	preload = () => {
+		this._roadImage = loadImage("images/road/road_bg.png");
+		this._backgroundImage = loadImage("images/start.gif");
+	};
 
-  setup = () => {
-    // Make multiple road, so they can look like scrolling
-    for (let i = 0; i < 3; i++) {
-      let yPos = i * this._roadHeight;
-      let sprite = createSprite(width / 2, yPos);
-      sprite.addImage(this._roadImage);
-      this._roadSprites.push(sprite);
-    }
-  };
+	setup = () => {
+		createCanvas(GAME_WIDTH, GAME_HEIGHT);
 
-  update = () => {
-    // Update the position of the road
-    this._roadSprites.forEach((roadSprite, i) => {
-      roadSprite.position.y += this._backgroundSpeed;
-    });
+		// Make multiple roads, so they can look like scrolling
+		for (let i = 0; i < 3; i++) {
+			let yPos = i * this._roadHeight;
+			let sprite = createSprite(width / 2, yPos);
+			sprite.addImage(this._roadImage);
+			sprite.collider = 'n'; //none
+			this._roadSprites.push(sprite);
+		}
+	};
 
-    this._repositionRoadsIfNeed();
-  };
+	update = () => {
+		/* 顯示背景 */
+		background(this._backgroundImage);
+		this._repositionRoadsIfNeed();
+	};
 
-  setBackgroundSpeed = (newSpeed) => {
-    this._backgroundSpeed = newSpeed;
-  };
+	getSection = () => {
+		return this._section;
+	};
 
-  getBackgroundSpeed = () => {
-    console.log(this._backgroundSpeed);
-    return this._backgroundSpeed;
-  };
+	setSection = (newSection) => {
+		const oldSection = this._section;
+		this._section = newSection;
 
-  // Check if the road needs to be repositioned based on scroll direction
-  _repositionRoadsIfNeed = () => {
-    this._roadSprites.forEach((roadSprite, i) => {
-      let halfRoadHeight = this._roadHeight / 2;
+		this._sectionChangedCallbacks.forEach((callback) => {
+			callback(oldSection, newSection);
+		})
+	};
 
-      if (this._backgroundSpeed > 0) {
-        let isOutOfBottomScreen =
-          roadSprite.position.y > height + halfRoadHeight;
-        if (isOutOfBottomScreen) {
-          let nextIndex = (i + 1) % this._roadSprites.length;
-          roadSprite.position.y =
-            this._roadSprites[nextIndex].position.y - this._roadHeight;
-        }
-      } else if (this._backgroundSpeed < 0) {
-        let isOutOfTopScreen = roadSprite.position.y < -halfRoadHeight;
-        if (isOutOfTopScreen) {
-          let prevIndex =
-            (i - 1 + this._roadSprites.length) % this._roadSprites.length;
-          roadSprite.position.y =
-            this._roadSprites[prevIndex].position.y + this._roadHeight;
-        }
-      }
-    });
-  };
+    // Method to register callbacks for section change
+    addSectionChangedCallback = (callback) => {
+        this._sectionChangedCallbacks.push(callback);
+    };
+
+	/**
+	 * Set camera follow on player position
+	 * @param {p5.vector} followPoint 
+	 */
+	cameraFollow = (followPoint) => {
+		// Set camera follow player on y axis
+		let offsetY = -200;
+		camera.y = followPoint.y + offsetY;
+	}
+	
+	/**
+	 * Return the bounds of the road sprite (minX, maxX)
+	 * @returns {number[]} [leftBound, rightBound]
+	 */
+	getRoadXRange = () => {
+		return [
+			this._roadSprites[0].position.x - this._roadSprites[0].width / 2,
+			this._roadSprites[0].position.x + this._roadSprites[0].width / 2,
+		];
+	}
+
+	/**
+	 * Based on camera current position, return the visible y range of the screen
+	 * @returns {number[]} [minY, maxY]
+	 */
+	getVisibleYRange = () => {
+		return [
+			camera.position.y - height / 2,
+			camera.position.y + height / 2
+		]
+	}
+
+	// Check if the road needs to be repositioned based on scroll direction
+	_repositionRoadsIfNeed = () => {
+		let player = playerController.getPlayer();
+		let [minY, maxY] = this.getVisibleYRange();
+		this._roadSprites.forEach((roadSprite, i) => {
+			let halfRoadHeight = this._roadHeight / 2;
+
+			if (player.vel.y < 0) {
+				let isOutOfBottomScreen =
+					roadSprite.position.y > maxY + halfRoadHeight;
+				if (isOutOfBottomScreen) {
+					let nextIndex = (i + 1) % this._roadSprites.length;
+					roadSprite.position.y =
+						this._roadSprites[nextIndex].position.y - this._roadHeight;
+				}
+			} else if (player.vel.y > 0) {
+				let isOutOfTopScreen = roadSprite.position.y < minY - halfRoadHeight;
+				if (isOutOfTopScreen) {
+					let prevIndex =
+						(i - 1 + this._roadSprites.length) % this._roadSprites.length;
+					roadSprite.position.y =
+						this._roadSprites[prevIndex].position.y + this._roadHeight;
+				}
+			}
+		});
+	};
 }
