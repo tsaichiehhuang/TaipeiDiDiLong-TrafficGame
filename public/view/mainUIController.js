3870b0f07042b4d975f4e0b800b8e31e43837eab
pj <Pinjiang1220@gmail.com>
2023-12-30 18:09:28 +0800

    Add speed UI
    - 加入速度 UI

--- public/view/mainUIController.js
+++ public/view/mainUIController.js
@@ -1,26 +1,47 @@
+let textVar = 'testMepipu';
 class MainUIController {
 	constructor() {
 		this.taskText = '';
 		this.score = 0;
 		this.alertText = '';
 		this.showAlertFlag = false;
+		this.pointerImage = null;
 	}
 
 	preload = () => {
+		this.pointerImage = loadImage('images/main_ui/speed_pointer.png');
 	}
 
 	setup = () => {
 		textAlign(LEFT, TOP);
 		textSize(20);
 		fill(255);
+
+		this.speedPos = createVector(width - 110, height - 100);
+		this.speedColor = color(214, 76, 76);
+		this.speedSize = 150 // 儀表板大小
+		this.speedWeight = 15; // 儀表板外匡粗細
+
+		let pointerW =  75;
+		let pointerH =  20;
+		let flipper = new Sprite(this.speedPos.x, this.speedPos.y, pointerW, pointerH, 'n');
+		flipper.img = this.pointerImage;
+		flipper.offset.x = -pointerW/2;
+		this.flipper = flipper;
+
+		allSprites.remove(flipper); // TODO
 	}
 
 	update = () => {
+		push();
 		this._drawTaskText();
 		this._drawScore();
 		if (this.showAlertFlag) {
 			this._drawAlert();
 		}
+		this._drawSpeed();
+		this.flipper.draw();	
+		pop();
 	}
 
 	setTaskText = (text) => {
@@ -52,5 +73,25 @@ class MainUIController {
 		textSize(30);
 		textAlign(CENTER, CENTER);
 		text(this.alertText, width / 2, height / 2);
+	}
+
+	_drawSpeed = () => {
+		// 畫出圓形外框
+		stroke(this.speedColor);
+		strokeWeight(this.speedWeight);
+		noFill();
+		let offsetAngle = 15;
+		arc(this.speedPos.x, this.speedPos.y, this.speedSize, this.speedSize, 180 - offsetAngle, 360 + offsetAngle);
+
+		// 依照速度畫出指針
+		let angle = this._speedToAngle(player.vel);
+		this.flipper.rotateTo(angle, 5);
+	}
+
+	_speedToAngle = (speed) => { 
+		let minSpeed = 0;
+		let maxSpeed = 5; // TODO: 配合玩家的速度上限調整
+		let speedMag = speed.mag();
+		return map(speedMag, minSpeed, maxSpeed, 0, 180);
 	}
 }
