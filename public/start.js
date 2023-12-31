82a81197906e8cf70be0c3008c0f2c97649ad017
Afan Chen <afan890918@gmail.com>
2023-12-31 14:34:16 +0800

    Adjust game screen size to (1280, 720)
    ## Description
    This commit modifies the game screen size, setting the width to 1280 pixels and the height to 720 pixels. This adjustment is made to accommodate different screen resolutions and provide an improved gaming experience.
    
    ## Changes Made
    - Adjust game screen size to (1280, 720)
    
    ## Testing
    Ensure testing across screens with different resolutions to confirm proper display of the game screen.
    
    ## Notes
    - The current game design may require further adjustments to adapt to the new screen size.
    - Pay attention to UI elements in the game to ensure they display correctly with the new screen size.

--- public/start.js
+++ public/start.js
@@ -15,19 +15,19 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(1000, 600);
+    createCanvas(1280, 720);
     background(background_img);
 
     // button:遊戲介紹
     intro_button = createImg(intro_game_button_path);
-    intro_button.position(390, 500);
+    intro_button.position(500, 600);
     intro_button.mousePressed(() => intro_started = true);
     intro_button.mouseOver(() => intro_button.attribute('src', intro_game_button_path.replace('1.png', '2.png')));
     intro_button.mouseOut(() => intro_button.attribute('src', intro_game_button_path));
 
     // button:遊戲開始
     start_button = createImg(game_started_button_path);
-    start_button.position(390, 400);
+    start_button.position(500, 500);
     start_button.mousePressed(() => game_started = true);
     start_button.mouseOver(() => start_button.attribute('src', game_started_button_path.replace('1.png', '2.png')));
     start_button.mouseOut(() => start_button.attribute('src', game_started_button_path));
