4b5fc33edb4ed6aa3372e7b84222ec4f88e217c1
Afan Chen <afan890918@gmail.com>
2023-12-31 13:33:28 +0800

    change button color

--- public/start.js
+++ public/start.js
@@ -1,7 +1,7 @@
 // 變數 ===========================================
 let background_img_path = './images/start/Main_bg.png'; // 封面
-let intro_game_button_path = './images/start/Howtoplay_2.png'
-let game_started_button_path = './images/start/Play_2.png'
+let intro_game_button_path = './images/start/Howtoplay_1.png'
+let game_started_button_path = './images/start/Play_1.png'
 
 // let game_title_path='./images/UI/start/title.png';
 
@@ -22,14 +22,14 @@ function setup() {
     intro_button = createImg(intro_game_button_path);
     intro_button.position(390, 500);
     intro_button.mousePressed(() => intro_started = true);
-    intro_button.mouseOver(() => intro_button.attribute('src', intro_game_button_path.replace('2.png', '1.png')));
+    intro_button.mouseOver(() => intro_button.attribute('src', intro_game_button_path.replace('1.png', '2.png')));
     intro_button.mouseOut(() => intro_button.attribute('src', intro_game_button_path));
 
     // button:遊戲開始
     start_button = createImg(game_started_button_path);
     start_button.position(390, 400);
     start_button.mousePressed(() => game_started = true);
-    start_button.mouseOver(() => start_button.attribute('src', game_started_button_path.replace('2.png', '1.png')));
+    start_button.mouseOver(() => start_button.attribute('src', game_started_button_path.replace('1.png', '2.png')));
     start_button.mouseOut(() => start_button.attribute('src', game_started_button_path));
 }
 
