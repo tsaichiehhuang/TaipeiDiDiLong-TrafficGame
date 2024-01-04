1864b509a7c03a78041036afef406765ad181f1d
chengyen yeh <a0987206119@gmal.com>
2024-01-04 21:58:38 +0800

    add Level - cross the road, Violation - not yield person

---
+++ public/manager/crossTheRoadManager.js
@@ -0,0 +1,20 @@
+class CrossTheRoadManager {
+
+    setup = () => {
+        this._stopped = loadImage('../images/text/text1.png');
+        this._notStopped = loadImage('../images/text/text2.png');
+    };
+
+    draw = (isStoppedInLevel) => {
+        let xCurrPosi = (gameManager.getRoadXRange()[0] + gameManager.getRoadXRange()[1]) / 2;
+        let yCurrPosi = (gameManager.getVisibleYRange()[0] + gameManager.getVisibleYRange()[1]) / 2;
+        imageMode(CENTER); // 把圖的正中央當定位點
+        
+        if (isStoppedInLevel === "stopped") {
+            image(this._stopped, xCurrPosi, yCurrPosi);
+        }else {
+            image(this._notStopped, xCurrPosi, yCurrPosi);
+        }
+
+    };
+}
