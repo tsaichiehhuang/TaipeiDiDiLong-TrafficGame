f5380fe3e72394ea7c4504633bef44065ec51bed
Yi-Jhen Jie <emily6331@gmail.com>
2024-01-04 15:12:46 +0800

    change traffic light event text image

--- public/manager/runRedLightManager.js
+++ public/manager/runRedLightManager.js
@@ -4,7 +4,7 @@ class RunRedLightManager {
     setup = () => {
         this._successText = loadImage("../images/text/text1.png");
         this._failText = loadImage("../images/text/text2.png");
-        this._failText2 = loadImage("../images/text/text2.png");
+        this._failText2 = loadImage("../images/text/text5.png");
     };
 
     draw = (status) => {
