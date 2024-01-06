7235213b454fad208eb382665351531f51e70ea4
Afan Chen <afan890918@gmail.com>
2024-01-06 17:22:52 +0800

    add play rule

--- public/start.js
+++ public/start.js
@@ -1,7 +1,8 @@
 // 變數 ===========================================
 let background_img_path = './images/start/Main_bg.png'; // 封面
-let intro_game_button_path = './images/start/Howtoplay_1.png'
-let game_started_button_path = './images/start/Play_1.png'
+let intro_game_button_path = './images/start/Howtoplay_1.png';
+let game_started_button_path = './images/start/Play_1.png';
+let how_to_play_path = './images/start/howtoplay.png';
 
 // let game_title_path='./images/UI/start/title.png';
 
@@ -12,6 +13,7 @@ let game_started = false;
 // p5js ==========================================
 function preload() {
     background_img = loadImage(background_img_path);
+    how_to_play = loadImage(how_to_play_path);
 }
 
 function setup() {
@@ -21,7 +23,7 @@ function setup() {
     // button:遊戲介紹
     intro_button = createImg(intro_game_button_path);
     intro_button.position(530, 600);
-    intro_button.mousePressed(() => intro_started = true);
+    intro_button.mousePressed(showHowToPlay);
     intro_button.mouseOver(() => intro_button.attribute('src', intro_game_button_path.replace('1.png', '2.png')));
     intro_button.mouseOut(() => intro_button.attribute('src', intro_game_button_path));
 
@@ -35,12 +37,33 @@ function setup() {
 
 function draw() {
     if (intro_started) {
-        
-    } else {
-
+        // 在指定位置顯示圖片
+        image(how_to_play, 0, 0, width, height);
     }
 
     if (game_started) {
-        window.location.href = "open.html" ;
+        window.location.href = "open.html";
+    }
+}
+
+function showHowToPlay() {
+    intro_started = true;
+    
+    // 隱藏介紹按鈕
+    intro_button.hide();
+    // 隱藏遊戲開始按鈕
+    start_button.hide();
+}
+
+function mousePressed() {
+    // 檢查是否點擊了圖片的右上角區域
+    if (intro_started && mouseX > width - 130 && mouseY < 130) {
+        background(background_img);
+        intro_started = false;
+        
+        // 重新顯示介紹按鈕
+        intro_button.show();
+        // 重新顯示遊戲開始按鈕
+        start_button.show();
     }
 }
