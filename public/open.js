b54eac3a4e49957fd7872300b85cc46a6d52654b
pj <Pinjiang1220@gmail.com>
2024-01-06 16:03:21 +0800

    Add button sound in opening and prepare bgm

--- public/open.js
+++ public/open.js
@@ -1,15 +1,20 @@
 const IMG_Count = 7;
-let openingImgs = [];
-let currentImg = 0;
 const x = 1020;
 const y = 620;
 const w = 180;
 const h = 40;
-
+let currentImg = 0;
+let openingImgs = [];
+let buttonSound;
+let bgm;
 function preload() {
     for (let i = 1; i <= IMG_Count; i++) {
         openingImgs.push(loadImage(`../images/opening/${i}.png`));
     }
+    buttonSound = loadSound("../audio/按鈕.mp3");
+    buttonSound.setVolume(0.5);
+    bgm = loadSound("../audio/BGM.mp3");
+    bgm.setVolume(0.5);
 }
 
 function setup() {
@@ -18,6 +23,7 @@ function setup() {
 }
 
 function draw() {
+
     // rect(1020, 620, 180, 40); // Button position
     if (mouseX < x || mouseX > x + w || mouseY < y || mouseY > y + h) {
         cursor(ARROW);
@@ -26,12 +32,26 @@ function draw() {
     }
 }
 
+function keyPressed() {
+    if (keyCode === ENTER) {
+        next();
+    }
+}
+
 function mousePressed() {
+    // if(!bgm.isPlaying()) {
+    //     bgm.play();
+    // }
+    
     // Check if mouse is pressed on button
     if (mouseX < x || mouseX > x + w || mouseY < y || mouseY > y + h) {
         return;
     }
+    next();
+}
 
+function next() {
+    buttonSound.play();
     currentImg++;
     if (currentImg >= IMG_Count) {
         window.location.href = "game.html";
