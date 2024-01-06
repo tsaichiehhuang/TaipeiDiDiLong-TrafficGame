274e4412b83a987ff417101dfcfc600bd0e5f288
pj <Pinjiang1220@gmail.com>
2024-01-06 22:02:34 +0800

    Hide red UI when keypressed is stop

--- public/manager/keyPressedManager.js
+++ public/manager/keyPressedManager.js
@@ -6,6 +6,9 @@ class KeyPressedManager {
     setKeyPressedStop(status) {
         // status = true or false
         this.keyPressedStop = status;
+
+        // 停止顯示紅色 UI，玩家不能移動且 UI 也不會被檢舉遮住
+        mainUIController.setIsShowing(!status);
     };
 
     getKeyPressedStop() {
